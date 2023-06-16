e74f0433fdb2cd42f9af9a5897ddfae4dd92ca14
Roshan <roshan.jha@hih7.in>
2023-06-16 16:17:48 +0530

    Nested Objects

--- src/components/YouTubeForm.tsx
+++ src/components/YouTubeForm.tsx
@@ -6,6 +6,10 @@ type Formvalues = {
   username: string;
   email: string;
   channel: string;
+  social: {
+    twitter: string;
+    facebook: string;
+  };
 };
 
 const YouTubeForm = () => {
@@ -15,6 +19,10 @@ const YouTubeForm = () => {
       username: "",
       email: "",
       channel: "",
+      social: {
+        twitter: "",
+        facebook: "",
+      },
     },
   });
 
@@ -83,6 +91,14 @@ const YouTubeForm = () => {
             })}
           />
           <p className="error">{errors.channel?.message}</p>
+        </div>
+        <div className="form-control">
+          <label htmlFor="twitter">Twitter</label>
+          <input type="text" id="twitter" {...register("social.twitter")} />
+        </div>
+        <div className="form-control">
+          <label htmlFor="facebook">Facebook</label>
+          <input type="text" id="facebook" {...register("social.facebook")} />
         </div>
         <button>Submit</button>
       </form>
