c59637eb38e85660b42c3b7136f4c4682fae251a
Roshan <roshan.jha@hih7.in>
2023-06-16 11:23:06 +0530

    DevTools Visualization

--- src/components/YouTubeForm.tsx
+++ src/components/YouTubeForm.tsx
@@ -1,20 +1,24 @@
 import { useForm } from "react-hook-form";
+import { DevTool } from "@hookform/devtools";
 
 const YouTubeForm = () => {
   const form = useForm();
 
-  const { register } = form;
+  const { register, control } = form;
 
   return (
-    <form>
-      <label htmlFor="username">UserName</label>
-      <input type="text" id="username" {...register("username")} />
-      <label htmlFor="email">Email</label>
-      <input type="email" id="email" {...register("email")} />
-      <label htmlFor="channel">Channel</label>
-      <input type="text" id="channel" {...register("channel")} />
-      <button>Submit</button>
-    </form>
+    <div>
+      <form>
+        <label htmlFor="username">UserName</label>
+        <input type="text" id="username" {...register("username")} />
+        <label htmlFor="email">Email</label>
+        <input type="email" id="email" {...register("email")} />
+        <label htmlFor="channel">Channel</label>
+        <input type="text" id="channel" {...register("channel")} />
+        <button>Submit</button>
+      </form>
+      <DevTool control={control} />
+    </div>
   );
 };
 
