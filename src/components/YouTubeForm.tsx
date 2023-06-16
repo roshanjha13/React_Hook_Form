3e07735a27364d476476738324737dc66152de07
Roshan <roshan.jha@hih7.in>
2023-06-16 10:53:32 +0530

    form make using html and css

---
+++ src/components/YouTubeForm.tsx
@@ -0,0 +1,15 @@
+const YouTubeForm = () => {
+  return (
+    <form>
+      <label htmlFor="username">UserName</label>
+      <input type="text" id="username" name="username" />
+      <label htmlFor="email">Email</label>
+      <input type="email" id="email" name="email" />
+      <label htmlFor="channel">Channel</label>
+      <input type="text" id="channel" name="cahnnel" />
+      <button>Submit</button>
+    </form>
+  );
+};
+
+export default YouTubeForm;
