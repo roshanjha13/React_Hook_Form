886972b98a257fc5348629ff6d46833dd397127a
Roshan <roshan.jha@hih7.in>
2023-06-19 12:38:02 +0530

    Async Validation

--- src/components/YouTubeForm.tsx
+++ src/components/YouTubeForm.tsx
@@ -122,8 +122,39 @@ const YouTubeForm = () => {
           <p className="error">{errors.username?.message}</p>
         </div>
         <div className="form-control">
-          <label htmlFor="email">Email</label>
-          <input type="email" id="email" {...register("email")} />
+          <label htmlFor="email">Email </label>
+          <input
+            type="email"
+            id="email"
+            {...register("email", {
+              pattern: {
+                value:
+                  /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/,
+                message: "Invalid email format",
+              },
+              validate: {
+                notAdmin: (fieldValue) => {
+                  return (
+                    fieldValue !== "admin@example.com" ||
+                    "Enter a different email address"
+                  );
+                },
+                notBlackListed: (fieldValue) => {
+                  return (
+                    !fieldValue.endsWith("baddomain.com") ||
+                    "This domain is not supported"
+                  );
+                },
+                emailAvailable: async (fieldValue) => {
+                  const response = await fetch(
+                    `https://jsonplaceholder.typicode.com/users?email=${fieldValue}`
+                  );
+                  const data = await response.json();
+                  return data.length == 0 || "Email already exists";
+                },
+              },
+            })}
+          />
           <p className="error">{errors.email?.message}</p>
         </div>
         <div className="form-control">
